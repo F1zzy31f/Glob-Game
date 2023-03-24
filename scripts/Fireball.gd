92c319e27e0ff5ad177574c8ff106bcb4ba2f94c
F1zzy31f <59973761+F1zzy31f@users.noreply.github.com>
2023-03-24 21:01:21 +0000

    Fix fireball authority, again

--- scripts/Fireball.gd
+++ scripts/Fireball.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 @onready var blast_radius = $BlastRadius
 
 func _on_body_entered(body):
+	if not is_multiplayer_authority(): return
+	
 	for hit in blast_radius.get_overlapping_bodies():
 		if hit.is_in_group("Hurtable"):
 			hit.hurt(32)
