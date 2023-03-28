06575234ac85c57b78b954c0d1f78cafb6688718
F1zzy31f <59973761+F1zzy31f@users.noreply.github.com>
2023-03-28 13:30:31 +0100

    Ultimate charger dormancy

--- scripts/UltimateCharger.gd
+++ scripts/UltimateCharger.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+@export var dormant = false
+
 func _on_detection_area_body_entered(body):
 	if body and body.is_in_group("Player"):
 		if body.charge_ultimate():
@@ -7,4 +9,8 @@ func _on_detection_area_body_entered(body):
 
 @rpc("any_peer", "call_local")
 func on_use():
+	dormant = true
+	
+	await get_tree().create_timer(1).timeout
+	
 	queue_free()
