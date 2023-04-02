306ba622dc838d170acb35b0a7659d52107b528e
F1zzy31f <59973761+F1zzy31f@users.noreply.github.com>
2023-04-02 10:24:08 +0100

    Update ult charger

--- scripts/UltimateCharger.gd
+++ scripts/UltimateCharger.gd
@@ -1,12 +1,22 @@
 extends RigidBody2D
 
-@export var dormant = false
+@export var explosion_effect = preload("res://scenes/ExplosionEffect.tscn")
 
 func _on_detection_area_body_entered(body):
 	if body and body.is_in_group("Player"):
 		if body.charge_ultimate():
-			on_use.rpc()
+			destroy.rpc()
 
 @rpc("any_peer", "call_local")
-func on_use():
+func destroy():
+	visible = false
+	set_deferred("freeze", true)
+	get_node("CollisionShape2D").set_deferred("disabled", true)
+	
+	var new_explosion = explosion_effect.instantiate()
+	new_explosion.global_position = global_position
+	Temporary.add_child(new_explosion)
+	
+	await get_tree().create_timer(1).timeout
+	
 	queue_free()
