575434dd72e239762295f2a5e55e5c3756a889b0
F1zzy31f <59973761+F1zzy31f@users.noreply.github.com>
2023-04-26 18:52:48 +0100

    Better item logging

--- scripts/Item.gd
+++ scripts/Item.gd
@@ -7,14 +7,14 @@ class_name Item
 var is_equip = false
 
 func reset():
-	print("Item reset not implemented")
+	Logger.log_simple("ITEM", "Item reset not implemented")
 
 func on_equip():
-	print("Item equip not implemented")
+	Logger.log_simple("ITEM", "Item equip not implemented")
 
 func on_unequip():
-	print("Item unequip not implemented")
+	Logger.log_simple("ITEM", "Item unequip not implemented")
 
 func get_item_info():
-	print("Item get info not implemented")
+	Logger.log_simple("ITEM", "Item get info not implemented")
 	return "Not implemented"
